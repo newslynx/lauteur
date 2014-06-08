365ffd4ff7229e0edda674d5eaec51f8525981d2
abelsonlive <brianabelson@gmail.com>
2014-06-08 11:37:41 -0400

    weird tests passing locally but not on travis

--- tests/tests.py
+++ tests/tests.py
@@ -3,10 +3,8 @@ import os
 
 import lauteur
 
-
 TEST_DIR = os.path.abspath(os.path.dirname(__file__))
 FIXTURES_DIR = os.path.join(TEST_DIR, 'fixtures')
-
 
 class Tests(unittest.TestCase):
   
@@ -19,6 +17,5 @@ class Tests(unittest.TestCase):
   def test_from_html(self):
     with open(os.path.join(FIXTURES_DIR, 'test.html'), 'rb') as f:
       html = f.read()
-    print html
     result = ['Michiko Kakutani']
     assert(str(lauteur.from_html(html)) == str(result))
