94a9eaa5276e93ff6b12d33cedd6b5e3b8ce6408
abelsonlive <brianabelson@gmail.com>
2014-06-08 11:16:46 -0400

    vbump

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ required = [str(ir.req) for ir in parse_requirements("requirements.txt")]
 # setup
 setup(
   name='lauteur',
-  version='0.1.0',
+  version='0.0.1',
   description='Tools for ascribing authorship - to the chagrin of Barthes',
   long_description = long_description,
   classifiers=[
